a4a007c4584b078011af8a3d9e3e7bf130798144
philipmarnef <philip@bureaumarnef.be>
2020-06-09 16:26:57 +0200

    Ditched Docker and Parcel, back to gulp. Plus custom Kirby setup.

---
+++ html/public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+echo (new Kirby([
+  'roots' => [
+    'index'    => __DIR__,
+    'base'     => $base    = dirname(__DIR__),
+    'content'  => $base . '/content',
+    'site'     => $base . '/site',
+    'storage'  => $storage = $base . '/storage',
+    'accounts' => $storage . '/accounts',
+    'cache'    => $storage . '/cache',
+    'sessions' => $storage . '/sessions',
+  ],
+]))->render();
---
+++ subset-fonts.zsh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+setopt extended_glob
+rm ./html/public/assets/fonts/* 2>/dev/null
+cp ./src/fonts/*.woff* ./html/public/assets/fonts/
+wait
+glyphhanger --formats=woff2,woff --subset="$(dirname "$0")/html/public/assets/fonts/*"
+wait
+rm ./html/public/assets/fonts/^*subset.*
