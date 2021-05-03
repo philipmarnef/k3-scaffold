2c900de71f38dd602faf92678ac58a37e65807b8
philipmarnef <philip@bureaumarnef.be>
2021-05-03 21:05:37 +0200

    FIX: subset fonts script fails if html/public/assets/fonts doesn't exist

--- subset-fonts.zsh
+++ subset-fonts.zsh
@@ -2,6 +2,7 @@
 
 setopt extended_glob
 rm ./html/public/assets/fonts/* 2>/dev/null
+mkdir ./html/public/assets/fonts 2>/dev/null
 cp ./src/fonts/*.woff* ./html/public/assets/fonts/
 wait
 glyphhanger --formats=woff2,woff --subset="$(dirname "$0")/html/public/assets/fonts/*"
