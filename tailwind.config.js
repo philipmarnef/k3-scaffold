34844ae1a825f0de46a453c9ad8969722a7e3a2d
philipmarnef <philip@bureaumarnef.be>
2021-05-06 17:37:34 +0200

    Tailwind config: extend where appropriate

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,6 @@ module.exports = {
     },
   },
   theme: {
-    screens: {
-      'sm':  '640px',
-      'md':  '768px',
-      'lg':  '1024px',
-      'xl':  '1280px',
-      '2xl': '1400px',
-    },
     colors: {
     },
     fontFamily: {
@@ -37,30 +30,6 @@ module.exports = {
       '9': '2.25rem',
       '10': '2.5rem',
     },
-    spacing: {
-      px: '1px',
-      '0': '0',
-      '1': '0.25rem',
-      '2': '0.5rem',
-      '3': '0.75rem',
-      '4': '1rem',
-      '5': '1.25rem',
-      '6': '1.5rem',
-      '8': '2rem',
-      '10': '2.5rem',
-      '12': '3rem',
-      '16': '4rem',
-      '20': '5rem',
-      '24': '6rem',
-      '32': '8rem',
-      '40': '10rem',
-      '48': '12rem',
-      '56': '14rem',
-      '64': '16rem',
-      '80': '20rem',
-      '96': '24rem',
-      '128': '32rem',
-    },
     zIndex: {
       '-100': '-100',
       '-10': '-10',
@@ -79,7 +48,16 @@ module.exports = {
       '100': '100',
       'auto': 'auto',
     },
-    extend: {},
+    extend: {
+      screens: {
+        '2xl': '1400px',
+      },  
+      spacing: {
+        '80': '20rem',
+        '96': '24rem',
+        '128': '32rem',
+      },
+    },
   },
   variants: {},
   plugins: [],
