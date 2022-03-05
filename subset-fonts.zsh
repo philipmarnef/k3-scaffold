473b25c643a19c1a9bca4871c4faf46466dac941
Philip Marnef <philip@bureaumarnef.be>
2022-03-05 20:32:40 +0100

    Subset any type of font (.otf, .ttf,...)

--- subset-fonts.zsh
+++ subset-fonts.zsh
@@ -3,7 +3,7 @@
 setopt extended_glob
 rm ./html/public/assets/fonts/* 2>/dev/null
 mkdir ./html/public/assets/fonts 2>/dev/null
-cp ./src/fonts/*.woff* ./html/public/assets/fonts/
+cp ./src/fonts/* ./html/public/assets/fonts/
 wait
 glyphhanger --formats=woff2,woff --subset="$(dirname "$0")/html/public/assets/fonts/*"
 wait
