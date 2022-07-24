bf50180d45aba32f0685c5f8bb1cf260a2f0cc61
Philip Marnef <philip@bureaumarnef.be>
2022-07-24 12:35:49 +0200

    Language meta tags

--- html/site/snippets/head.php
+++ html/site/snippets/head.php
@@ -1,4 +1,7 @@
-<?php use Bnomei\Fingerprint; ?>
+<?php
+	use Kirby\Toolkit\Html;
+	use Bnomei\Fingerprint; 
+?>
 
 <head>
 
@@ -10,6 +13,25 @@
 <?php if($_SERVER['HTTP_HOST'] !== 'k3-scaffold.test'): ?>
 	<meta name="robots" content="noindex,nofollow,noodp">
 <?php endif; ?>
+
+	<link href="<?= $page->url() ?>" rel="canonical">
+	<link href="<?= $site->homePage()->url() ?>" rel="home">
+<?php foreach($kirby->languages() as $language) {
+	$href = $page->url($language->code());
+	$lang = $language->code();
+	echo "\t" . Html::tag('link', null, [
+		'href' => $href,
+		'rel' => 'alternate',
+		'hreflang' => $lang,
+	]) . "\n";
+	if($language->isDefault()) {
+		echo "\t" . Html::tag('link', null, [
+			'href' => $href,
+			'rel' => 'alternate',
+			'hreflang' => 'x-default',
+		]) . "\n";
+	}
+} ?>
 
 	<?= Fingerprint::css('assets/css/main.css') ?>
 	<?= Fingerprint::js('assets/js/main.js') ?>
