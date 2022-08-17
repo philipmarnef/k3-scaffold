8a86609f1517e8a88c319ecf5706ae6b6b7ef64e
Philip Marnef <philip@bureaumarnef.be>
2022-08-17 11:40:57 +0200

    Subset fonts script improvements

--- subset-fonts.zsh
+++ subset-fonts.zsh
@@ -2,9 +2,9 @@
 
 setopt extended_glob
 rm ./html/public/assets/fonts/* 2>/dev/null
-mkdir ./html/public/assets/fonts 2>/dev/null
+mkdir -p ./html/public/assets/fonts 2>/dev/null
 cp ./src/fonts/* ./html/public/assets/fonts/
 wait
 glyphhanger --formats=woff2,woff --subset="$(dirname "$0")/html/public/assets/fonts/*"
 wait
-rm ./html/public/assets/fonts/^*subset.*
+rm ./html/public/assets/fonts/^*subset.* 2>/dev/null
