1c6d26a0b69d07fd9cc04435e647f3abb3078e40
Philip Marnef <philip@bureaumarnef.be>
2023-02-20 14:33:02 +0100

    Log folder location for kirbylog() (& others)

--- html/public/index.php
+++ html/public/index.php
@@ -3,14 +3,15 @@
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 echo (new Kirby([
-  'roots' => [
-    'index'    => __DIR__,
-    'base'     => $base    = dirname(__DIR__),
-    'content'  => $base . '/content',
-    'site'     => $base . '/site',
-    'storage'  => $storage = $base . '/storage',
-    'accounts' => $storage . '/accounts',
-    'cache'    => $storage . '/cache',
-    'sessions' => $storage . '/sessions',
-  ],
+	'roots' => [
+		'index'			=> __DIR__,
+		'base'			=> $base    = dirname(__DIR__),
+		'content'		=> $base . '/content',
+		'site'			=> $base . '/site',
+		'storage'		=> $storage = $base . '/storage',
+		'accounts'	=> $storage . '/accounts',
+		'cache'			=> $storage . '/cache',
+		'sessions'	=> $storage . '/sessions',
+		'logs'			=> $storage . '/log',
+	],
 ]))->render();
