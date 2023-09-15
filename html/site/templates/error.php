45b0d876ca9a2b3eb62f25f621be22976445baa2
Philip Marnef <philip@bureaumarnef.be>
2023-09-15 16:37:12 +0200

    Added error template so I don't keep forgetting.

---
+++ html/site/templates/error.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="<?= $kirby->languageCode() ?>">
+<?= snippet('head') ?>
+
+<body>
+	<h1>404 – Page Not Found</h1>
+	<p>We couldn't find the page you're looking for.<br> <a href="<?= $site->homePage()->url() ?>">Go to the homepage</a> instead.</p>
+</body>
+
+</html>
